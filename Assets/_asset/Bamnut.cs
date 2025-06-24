62a7ae1d4985a8b136d5068a25227e4d7e3a5cda
Minh <qminh2702@gmail.com>
2025-06-24 08:16:37 +0700

    chuong 11 pt1 hoan thien

--- Assets/_asset/Bamnut.cs
+++ Assets/_asset/Bamnut.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bamnut : MonoBehaviour
 {
-
+    public GameObject MainMenu;
     public GameObject Message1;
     public GameObject Message2;
     public GameObject Message3;
@@ -19,84 +19,113 @@ public class Bamnut : MonoBehaviour
 
     public void OnButton1()
     {
+        MainMenu.SetActive(false);
         Message1.SetActive(true);
     }
     public void OnButton2()
     {
         Message2.SetActive(true);
+        MainMenu.SetActive(false);
     }
     public void OnButton3()
     {
         Message3.SetActive(true);
+        MainMenu.SetActive(false);
     }
     public void OnButton4()
     {
         Message4.SetActive(true);
+        MainMenu.SetActive(false);
     }
     public void OnButton5()
     {
         Message5.SetActive(true);
+        MainMenu.SetActive(false);
     }
     public void OnButton6()
     {
         Message6.SetActive(true);
+        MainMenu.SetActive(false);
     }
     public void OnButton7()
     {
         Message7.SetActive(true);
+        MainMenu.SetActive(false);
     }
     public void OnButton8()
     {
         Message8.SetActive(true);
+        MainMenu.SetActive(false);
     }
     public void OnButton9()
     {
         Message9.SetActive(true);
+        MainMenu.SetActive(false);
     }
     public void OnButton10()
     {
         Message10.SetActive(true);
+        MainMenu.SetActive(false);
     }
 
     public void OnBackButton1()
     {
         Message1.SetActive(false);
+        MainMenu.SetActive(true);
     }
     public void OnBackButton2()
     {
         Message2.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
     public void OnBackButton3()
     {
         Message3.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
     public void OnBackButton4()
     {
         Message4.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
     public void OnBackButton5()
     {
         Message5.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
     public void OnBackButton6()
     {
         Message6.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
     public void OnBackButton7()
     {
         Message7.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
     public void OnBackButton8()
     {
         Message8.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
     public void OnBackButton9()
     {
         Message9.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
     public void OnBackButton10()
     {
         Message10.SetActive(false);
+        MainMenu.SetActive(true);
+
     }
 
 }
